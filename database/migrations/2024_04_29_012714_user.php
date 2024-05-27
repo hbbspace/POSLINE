617d0208e6cde206e3dc3db2b2ba7bad57d2fedc
Habibatul Mustofa <habibatulmustofa1@gmail.com>
2024-05-28 00:10:23 +0700

    Update migration & seeder

--- database/migrations/2024_04_29_012714_user.php
+++ database/migrations/2024_04_29_012714_user.php
@@ -13,12 +13,13 @@ return new class extends Migration
     {
         Schema::create('user', function (Blueprint $table) {
             $table->increments('user_id');
-            $table->string('username', 16);
+            $table->string('nik', 16);
+            $table->string('username', 25);
             $table->string('password', 60);
             // Tambahkan kolom lain sesuai kebutuhan
 
             // Tambahkan foreign key
-            $table->foreign('username')->references('nik')->on('anggota_keluarga');
+            $table->foreign('nik')->references('nik')->on('anggota_keluarga');
         });
     }
 
